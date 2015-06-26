fa4c66929af50cce3c9a62375253eca02e9d700d
Long <guitar1212@gmail.com>
2015-06-26 12:49:44 +0800

    import project
    import project

---
+++ Project001/Assets/Scripts/AutoRotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoRotate : MonoBehaviour {
+
+	public float rotateSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate (new Vector3 (0, 1, 0), rotateSpeed);
+	}
+}
