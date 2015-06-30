6d4bea3d0690814108fb3d9cb8ddf3eb53061430
Long <guitar1212@gmail.com>
2015-06-30 14:39:38 +0800

    modify model prefab
    recreate model prefab

---
+++ Project001/Assets/Scripts/ModelManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModelManager : MonoBehaviour {
+
+	public Transform currentModel;
+
+	public Transform target;
+
+	public Transform[] models;
+
+	private int modexIdx = 0;
+
+	// Use this for initialization
+	void Start () {
+
+		modexIdx = 0;
+
+		if (currentModel && target) {
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (currentModel && target) {
+			currentModel.position = target.position;
+
+			//currentModel.rotation = target.rotation;
+		}
+	}
+
+	public void NextModel()
+	{
+		if (currentModel) {
+			Destroy(currentModel.gameObject);
+			currentModel = null;
+		}
+
+		modexIdx++;
+
+		if (modexIdx >= models.Length)
+			modexIdx = 0;
+
+		currentModel = Instantiate (models [modexIdx]);
+	}
+}
